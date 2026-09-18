fcbef058b810c11e4bd4d7c6be06cb2de2d9f877
OutTuna <glebbannikov201@gmail.com>
2026-09-18 22:31:50 +0300

    fix(state): защитить общие данные мьютексом и atomic, увеличить буферы путей

--- app_state.h
+++ app_state.h
@@ -2,23 +2,28 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <mutex>
+#include <atomic>
 
-//inline const std::string SETTINGS_FILE = "settings.json";
-//inline const std::string CACHE_FILE    = "nicknames.json";
-inline const std::string DOTA_ID       = "570";
+inline const std::string DOTA_ID = "570";
+inline constexpr size_t PATH_BUF_SIZE = 1024;
+inline char src_path[PATH_BUF_SIZE] = "";
+inline char dst_path[PATH_BUF_SIZE] =
+#ifdef _WIN32
+    "C:\\Program Files (x86)\\Steam\\userdata";
+#else
+    "";
+#endif
 
-inline char src_path[256] = "";
-inline char dst_path[256] = "C:\\Program Files (x86)\\Steam\\userdata";
-
+inline std::mutex g_data_mutex;
 inline std::map<std::string, std::string> nick_cache;
 inline std::vector<std::string> src_list;
 inline std::vector<std::string> dst_list;
-
 inline std::string status_msg = "Ready";
-inline int selected_src = -1;
-inline int selected_dst = -1;
-
-inline int  g_theme = 0;
+inline std::atomic<int> selected_src{-1};
+inline std::atomic<int> selected_dst{-1};
+inline std::atomic<bool> g_scanning{false};
+inline int g_theme = 0;
 
 struct SuccessInfo {
     bool        show     = false;
