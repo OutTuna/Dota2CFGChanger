ba65ec720aa227161697c920225bb06708957968
OutTuna <glebbannikov201@gmail.com>
2026-09-19 18:03:51 +0300

    fix(steam_api): фикс отображения автарок профиля

--- steam_api.h
+++ steam_api.h
@@ -4,4 +4,4 @@ namespace steam_api {
 long long steam3_to_64(const std::string& steam3_id);
 std::string fetch_profile_xml(long long steam64, int timeout_ms);
 std::string extract_tag(const std::string& xml, const std::string& tag);
-} 
+}
